7b76240fec758dae536244385a31100abb62ff5a
bryannbarbosa <bryannbarbosa@gmail.com>
2017-10-26 15:18:37 -0200

    book routes

---
+++ database/database.sql
@@ -0,0 +1,11 @@
+
+create database maciv_flipbook default charset 'utf8' default collate 'utf8_general_ci';
+
+use maciv_flipbook;
+create table books(
+id int not null auto_increment primary key,
+book_name varchar(100) not null,
+publish_date date not null,
+book_pdf_url text not null
+);
+
--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../database/config');
+const moment = require('moment');
+const mysql = require('mysql');
 
 router.get('/books', (req, res) => {
-    res.json({
-        response: 'This is a test',
-        success: true
+    connection.connect((err) => {
+        connection.query('select * from books', (err, result, fields) => {
+            if (err)
+            throw err;
+            res.json({
+                response: result,
+                success: true
+            });
+        });
+    });
+});
+
+router.post('/books', (req, res) => {
+    connection.connect((err) => {
+        let book = req.body.book_name;
+        let publish = moment().format('L');
+        let pdf = req.body.pdf;
+        let values = [
+            [req.body.book_name, publish, pdf]
+        ];
+        connection.query('insert into books (book_name, publish_date, book_pdf_url) values :params', { params: values }, (err, result, fields) => {
+            if (err)
+            throw err;
+            res.json({
+                response: 'Book inserted successfully',
+                id: result.insertId,
+                success: true
+            });
+        });
     });
 });
 
