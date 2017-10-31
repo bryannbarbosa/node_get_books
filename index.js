0a3217fd4ff30e38f0180b7c783ba97279cfb62b
bryannbarbosa <bryannbarbosa@gmail.com>
2017-10-30 22:42:38 -0200

    file upload

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
+const storage = require('./scripts/storage');
+const upload = require('./scripts/upload');
+const app = express();
+
+app.use("/uploads/documents", express.static(__dirname + '/uploads/documents'));
 
 const routes = [
-    require('./routes/books'),
+  require('./routes/books'),
 ];
 
-const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 routes.map(route => app.use('/api', route));
 
+
+
 const mysql = require('./database/config');
 
-app.get('/', (req, res) => {
-    res.send({name: 'Yoshi'});
-});
-
 app.listen(process.env.port || 4000, function() {
-    console.log('Server is running');
+  console.log('Server is running');
 });
--- routes/books.js
+++ routes/books.js
@@ -4,46 +4,111 @@ const connection = require('../database/config');
 const moment = require('moment');
 const mysql = require('mysql');
 
-router.get('/books', (req, res) => {
-    connection.connect((err) => {
-        connection.query('select * from books', (err, result, fields) => {
-            if (err)
-            throw err;
-            res.json({
-                response: result,
-                success: true
-            });
-        });
-    });
-});
-
-router.post('/books', (req, res) => {
-    if('book_name', 'pdf' in req.body) {
-        connection.connect((err) => {
-            let book = req.body.book_name;
-            let publish = moment().format('YYYY-MM-DD');
-            let pdf = req.body.pdf;
-            let values = [
-                [req.body.book_name, publish, pdf]
-            ];
-            connection.query('insert into books (book_name, publish_date, book_pdf_url) values ?', [values], (err, result, fields) => {
-                if (err)
-                throw err;
-                res.json({
-                    response: 'Book inserted successfully',
-                    id: result.insertId,
-                    success: true
-                });
-            });
-        });
-    }
-
-    else {
+router.get('/book', (req, res) => {
+  connection.connect((err) => {
+    connection.query('select * from books', (err, result, fields) => {
+      if (err)
         res.json({
-            response: 'book_name and pdf keys are required',
+            response: err,
             success: false
         })
-    }
+      res.json({
+        response: result,
+        success: true
+      });
+    });
+  });
+});
+
+router.get('/book/:id', (req, res) => {
+  connection.connect((err) => {
+    connection.query(`select * from books where id = ${req.params.id} `, (err, result, fields) => {
+      if (err)
+        res.json({
+            response: err,
+            success: false
+        })
+      res.json({
+        response: result,
+        success: true
+      });
+    });
+  });
+});
+
+router.post('/book', (req, res) => {
+  if ('book_name', 'pdf' in req.body) {
+    connection.connect((err) => {
+      let book = req.body.book_name;
+      let publish = moment().format('YYYY-MM-DD');
+      let pdf = req.body.pdf;
+      let values = [
+        [req.body.book_name, publish, pdf]
+      ];
+      connection.query('insert into books (book_name, publish_date, book_pdf_url) values ?', [values], (err, result, fields) => {
+        if (err)
+          res.json({
+              response: err,
+              success: false
+          })
+        res.json({
+          response: 'Book inserted successfully',
+          id: result.insertId,
+          success: true
+        });
+      });
+    });
+  } else {
+    res.json({
+      response: 'book_name and pdf keys are required',
+      success: false
+    })
+  }
+});
+
+router.put('/book/:id', (req, res) => {
+  if ('book_name', 'pdf' in req.body) {
+    connection.connect((err) => {
+      let book = req.body.book_name;
+      let pdf = req.body.pdf;
+      let values = [
+        [req.body.book_name, publish, pdf]
+      ];
+      connection.query(`update books set book_name  = '${book}', book_pdf_url = '${pdf}' where id = ${req.params.id} `, (err, result, fields) => {
+        if (err)
+          res.json({
+              response: err,
+              success: false
+          });
+          console.log(err);
+        res.json({
+          response: 'Book updated successfully',
+          success: true
+        });
+      });
+    });
+  } else {
+    res.json({
+      response: 'book_name and pdf keys are required',
+      success: false
+    })
+  }
+});
+
+router.delete('/book/:id', (req, res) => {
+  connection.connect((err) => {
+    connection.query(`delete from books where id = ${req.params.id}`, (err, result, fields) => {
+      if (err)
+        res.json({
+            response: err,
+            success: false
+        });
+      res.json({
+        response: 'Book deleted successfully',
+        success: true
+      });
+    });
+  });
 });
 
 module.exports = router;
